87053decc64b299777c4fa1754c9d215b82f9334
Leo Hong <5917188+low-earth-orbit@users.noreply.github.com>
2025-06-16 01:33:38 -0300

    update 0b

--- 0b-tictactoe/tictactoe.py
+++ 0b-tictactoe/tictactoe.py
@@ -2,6 +2,7 @@
 Tic Tac Toe Player
 """
 
+import copy
 import math
 
 X = "X"
@@ -13,30 +14,46 @@ def initial_state():
     """
     Returns starting state of the board.
     """
-    return [[EMPTY, EMPTY, EMPTY],
-            [EMPTY, EMPTY, EMPTY],
-            [EMPTY, EMPTY, EMPTY]]
+    return [[EMPTY, EMPTY, EMPTY], [EMPTY, EMPTY, EMPTY], [EMPTY, EMPTY, EMPTY]]
 
 
 def player(board):
     """
     Returns player who has the next turn on a board.
     """
-    raise NotImplementedError
+    # X gets the first move
+    o_count = sum(row.count(O) for row in board)
+    x_count = sum(row.count(X) for row in board)
+    if o_count >= x_count:
+        return X
+    else:
+        return O
 
 
 def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    raise NotImplementedError
+    # initialize actions set
+    actions = set()
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == EMPTY:
+                actions.add((i, j))
+    return actions
 
 
 def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    raise NotImplementedError
+    valid_actions = actions(board)
+    (i, j) = action
+    if (i, j) not in valid_actions:
+        raise ValueError
+    new_board = copy.deepCopy(board)  # clone the board so board is left intact
+    new_board[i][j] = player(board)
+    return new_board
 
 
 def winner(board):
@@ -50,18 +67,42 @@ def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    raise NotImplementedError
+    # If there is a winner return True
+    if winner(board) != None:
+        return True
+
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == EMPTY:
+                return False
+
+    # If all cells have been filled return True
+    return True
 
 
 def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    raise NotImplementedError
+    # this function will only be called if terminal(board) is True
+    win = winner(board)
+    if win == X:
+        return 1
+    if win == O:
+        return -1
+    return 0
 
 
 def minimax(board):
     """
     Returns the optimal action for the current player on the board.
     """
+    # return None is board is a terminal board
+    if terminal(board):
+        return None
+
+    valid_actions = actions(board)
+
+    current_player = player(board)
+
     raise NotImplementedError
