71937e20ba9e8e7a99b889018049f1e0f41e3002
Leo Hong <5917188+low-earth-orbit@users.noreply.github.com>
2025-06-16 21:31:52 -0300

    complete 1a

--- 1a-knights/puzzle.py
+++ 1a-knights/puzzle.py
@@ -9,24 +9,42 @@ BKnave = Symbol("B is a Knave")
 CKnight = Symbol("C is a Knight")
 CKnave = Symbol("C is a Knave")
 
+ASKnight = Symbol("A said I am a Knight")
+ASKnave = Symbol("A said I am a Knave")
+
 # Puzzle 0
 # A says "I am both a knight and a knave."
 knowledge0 = And(
-    # TODO
+    Or(AKnight, AKnave),
+    Not(And(AKnight, AKnave)),
+    Implication(AKnight, And(AKnight, AKnave)),
+    Implication(AKnave, Not(And(AKnight, AKnave))),
 )
 
 # Puzzle 1
 # A says "We are both knaves."
 # B says nothing.
 knowledge1 = And(
-    # TODO
+    Or(AKnight, AKnave),
+    Not(And(AKnight, AKnave)),
+    Or(BKnight, BKnave),
+    Not(And(BKnight, BKnave)),
+    Implication(AKnight, And(AKnave, BKnave)),
+    Implication(AKnave, Not(And(AKnave, BKnave))),
 )
 
 # Puzzle 2
 # A says "We are the same kind."
 # B says "We are of different kinds."
 knowledge2 = And(
-    # TODO
+    Or(AKnight, AKnave),
+    Not(And(AKnight, AKnave)),
+    Or(BKnight, BKnave),
+    Not(And(BKnight, BKnave)),
+    Implication(AKnight, Or(And(AKnight, BKnight), And(AKnave, BKnave))),
+    Implication(AKnave, Not(Or(And(AKnight, BKnight), And(AKnave, BKnave)))),
+    Implication(BKnight, Or(And(AKnight, BKnave), And(AKnave, BKnight))),
+    Implication(BKnave, Not(Or(And(AKnight, BKnave), And(AKnave, BKnight)))),
 )
 
 # Puzzle 3
@@ -35,7 +53,24 @@ knowledge2 = And(
 # B says "C is a knave."
 # C says "A is a knight."
 knowledge3 = And(
-    # TODO
+    Or(AKnight, AKnave),
+    Not(And(AKnight, AKnave)),
+    Or(BKnight, BKnave),
+    Not(And(BKnight, BKnave)),
+    Or(CKnight, CKnave),
+    Not(And(CKnight, CKnave)),
+    Implication(
+        AKnight, Or(ASKnight, ASKnave)
+    ),  # A says either "I am a knight." or "I am a knave.", but you don't know which.
+    Implication(
+        AKnave, Not(Or(ASKnight, ASKnave))
+    ),  # A says either "I am a knight." or "I am a knave.", but you don't know which.
+    Implication(BKnight, ASKnave),  # B says "A said 'I am a knave'."
+    Implication(BKnave, Not(ASKnave)),  # B says "A said 'I am a knave'."
+    Implication(BKnight, CKnave),  # B says "C is a knave."
+    Implication(BKnave, Not(CKnave)),  # B says "C is a knave."
+    Implication(CKnight, AKnight),  # C says "A is a knight."
+    Implication(CKnave, Not(AKnight)),  # C says "A is a knight."
 )
 
 
@@ -45,7 +80,7 @@ def main():
         ("Puzzle 0", knowledge0),
         ("Puzzle 1", knowledge1),
         ("Puzzle 2", knowledge2),
-        ("Puzzle 3", knowledge3)
+        ("Puzzle 3", knowledge3),
     ]
     for puzzle, knowledge in puzzles:
         print(puzzle)
